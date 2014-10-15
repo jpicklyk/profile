42b4b3787cf3e25df21dba4f6c7f7835b50005dd
jpicklyk <jeff_picklyk@hotmail.com>
2014-10-15 10:36:30 -0400

    Update active_directory.pp
    Removed specifying parameters here in favour of hiera auto-lookup

--- manifests/active_directory.pp
+++ manifests/active_directory.pp
@@ -1,22 +1,3 @@
 class profile::active_directory {
-  class { '::windows_ad':
-    install                => present,
-    installmanagementtools => true,
-    restart                => true,
-    installflag            => true,
-    configure              => present,
-    configureflag          => true,
-    domain                 => 'forest',
-    domainname             => 'gestest.net',
-    netbiosdomainname      => 'gestest',
-    domainlevel            => '4',
-    forestlevel            => '4',
-    databasepath           => 'c:\\windows\\ntds',
-    logpath                => 'c:\\windows\\ntds',
-    sysvolpath             => 'c:\\windows\\sysvol',
-    installtype            => 'domain',
-    dsrmpassword           => 'password',
-    installdns             => 'yes',
-    localadminpassword     => 'password',
-  }
+  include ::windows_ad
 }
