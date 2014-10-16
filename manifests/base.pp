f0e645cf339b9c2ded24b4bea9a7315678c8191a
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-16 16:08:46 -0400

    removed chocolatey_sw from branch

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,8 +2,7 @@ class profile::base {
   if $operatingsystem == 'Windows' {
     include profile::iesec
     include windows_puppet
-    include chocolatey_sw
-  
+      
   } elsif $operatingsystem == 'Ubuntu' {
     
   }
