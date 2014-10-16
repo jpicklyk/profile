e179c9005520a4c175bf06fa30dee41f9e104e4f
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-16 16:46:05 -0400

    Making sure NotePad++ gets installed

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,6 +3,13 @@ class profile::base {
     include profile::iesec
     include windows_puppet
     include chocolatey_sw
+    
+    #Ensure Notepad++ is installed
+    package { 'notepadplusplus':
+      ensure => present,
+      provider => 'chocolatey',
+      require => Class['chocolatey_sw']
+    }
       
   } elsif $operatingsystem == 'Ubuntu' {
     
