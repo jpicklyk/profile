e882a8e03621492ae57a11f29e8419a560edc2fd
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-17 08:59:44 -0400

    Changing applications to be defined in a variable.  Can be overriden by hiedra.

--- manifests/win_default_apps.pp
+++ manifests/win_default_apps.pp
@@ -1,46 +1,13 @@
-class profile::win_default_apps {
+class profile::win_default_apps(
+    $applications = ["notepadplusplus.install","wireshark", "sysinternals","7zip.install","GoogleChrome","windirstat"]
+  ) {
+    
   include chocolatey_sw
-
-  # Ensure Notepad++ is installed
-  package { 'notepadplusplus.install':
-    ensure   => present,
-    provider => 'chocolatey',
-    require  => Class['chocolatey_sw']
-  }
-
-  # Ensure wireshark is installed
-  package { 'wireshark':
+  
+  package { $applications:
     ensure   => present,
     provider => 'chocolatey',
     require  => Class['chocolatey_sw']
   }
 
-  # Ensure sysinternals is installed
-  package { 'sysinternals':
-    ensure   => present,
-    provider => 'chocolatey',
-    require  => Class['chocolatey_sw']
-  }
-  
-  # Ensure 7zip is installed
-  package { '7zip.install':
-    ensure   => present,
-    provider => 'chocolatey',
-    require  => Class['chocolatey_sw']
-  }
-  
-  # Ensure Google Chrome is installed
-  package { 'GoogleChrome':
-    ensure   => present,
-    provider => 'chocolatey',
-    require  => Class['chocolatey_sw']
-  }
-  
-  # Ensure WinDirStat is installed
-  package { 'windirstat':
-    ensure   => present,
-    provider => 'chocolatey',
-    require  => Class['chocolatey_sw']
-  }
-  
 }
