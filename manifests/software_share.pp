8008ffbdc25360a0fdffdf0030df435d927fda19
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-23 08:43:33 -0400

    Requiring default apps including powershell 4

--- manifests/software_share.pp
+++ manifests/software_share.pp
@@ -5,6 +5,8 @@ class profile::software_share (
   $account    = hiera("profile::software_share::account"),
   $key        = hiera("profile::software_share::key"),
 ) {
+  require profile::win_default_apps
+  
   $credential = "(New-Object System.Management.Automation.PsCredential(\'${account}\', (ConvertTo-SecureString \'${key}\' -AsPlainText -Force)))"
   exec { 's-drive':
     command   => "New-PSDrive -Name ${drive} -PSProvider ${psprovider} -Root ${root} -Credential ${credential} -Persist",
