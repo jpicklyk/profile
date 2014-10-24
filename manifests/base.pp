5e02d01105bc609a21c4884cd7d7c1e9a214a9aa
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-24 16:04:04 -0400

    Adding powershell back

--- manifests/base.pp
+++ manifests/base.pp
@@ -5,9 +5,9 @@ class profile::base {
     #contain windows_puppet
     #contain profile::win_default_apps
     contain profile::software_share
-    #contain profile::powershell
+    contain profile::powershell
     
-    Class['profile::iesec'] -> Class['profile::software_share'] #-> Class['profile::powershell'] #-> Class['profile::win_default_apps'] 
+    Class['profile::iesec'] -> Class['profile::software_share'] -> Class['profile::powershell'] #-> Class['profile::win_default_apps'] 
     
   } elsif $operatingsystem == 'Ubuntu' {
     
