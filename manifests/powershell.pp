856e3db1d040a762e2d01b9a47f2dd07076f3b4e
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-24 17:03:35 -0400

    Had cmd in the wrong spot

--- manifests/powershell.pp
+++ manifests/powershell.pp
@@ -11,8 +11,9 @@ class profile::powershell(
 #      install_options => ['/quiet','/norestart'],
 #    }
     exec {'powershell4':
-      command => "cmd /c 'c:\\windows\\system32\\wusa.exe ${share}${filename} /quiet /norestart'",
-      onlyif => "$::systemroot\\system32\\WindowsPowershell\\v1.0\\powershell.exe -executionpolicy remotesigned if(\$PSVersionTable -and (\$PSVersionTable.PSVersion -ge [Version]'4.0')){exit 1}"
+      command => "c:\\windows\\system32\\wusa.exe ${share}${filename} /quiet /norestart",
+      onlyif => "cmd /c '$::systemroot\\system32\\WindowsPowershell\\v1.0\\powershell.exe -executionpolicy remotesigned if(\$PSVersionTable -and (\$PSVersionTable.PSVersion -ge [Version]'4.0')){exit 1}'"
+      
     } 
   }
 }
