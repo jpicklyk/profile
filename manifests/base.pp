a9805f1c009dd7a3c1592ee6c50ba55b9ac869ea
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-24 15:59:27 -0400

    Test

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,12 +2,12 @@ class profile::base {
   notify {'Applying profile: base':}
   if $operatingsystem == 'Windows' {
     contain profile::iesec
-    contain windows_puppet
-    contain profile::win_default_apps
+    #contain windows_puppet
+    #contain profile::win_default_apps
     contain profile::software_share
-    contain profile::powershell
+    #contain profile::powershell
     
-    Class['profile::iesec'] -> Class['profile::software_share'] -> Class['profile::powershell'] -> Class['windows_puppet'] -> Class['profile::win_default_apps'] 
+    Class['profile::iesec'] -> Class['profile::software_share'] #-> Class['profile::powershell'] #-> Class['profile::win_default_apps'] 
     
   } elsif $operatingsystem == 'Ubuntu' {
     
