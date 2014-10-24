1efb9e875b937f886e7ddde5d662b62725a673e7
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-24 15:50:11 -0400

    removing dependency

--- manifests/software_share.pp
+++ manifests/software_share.pp
@@ -5,7 +5,7 @@ class profile::software_share (
   $account    = hiera("profile::software_share::account"),
   $key        = hiera("profile::software_share::key"),
 ) {
-  require profile::win_default_apps
+  
   
   $credential = "(New-Object System.Management.Automation.PsCredential(\'${account}\', (ConvertTo-SecureString \'${key}\' -AsPlainText -Force)))"
   exec { 's-drive':
