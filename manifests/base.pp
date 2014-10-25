cd90ee9511b68f3459ef8957a5c07d7cc272bc3e
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-25 10:48:28 -0400

    Adding default apps back

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,11 +3,11 @@ class profile::base {
   if $operatingsystem == 'Windows' {
     contain profile::iesec
     #contain windows_puppet
-    #contain profile::win_default_apps
+    contain profile::win_default_apps
     contain profile::software_share
     contain profile::powershell
     
-    Class['profile::iesec'] -> Class['profile::software_share'] -> Class['profile::powershell'] #-> Class['profile::win_default_apps'] 
+    Class['profile::iesec'] -> Class['profile::software_share'] -> Class['profile::powershell'] -> Class['profile::win_default_apps'] 
     
   } elsif $operatingsystem == 'Ubuntu' {
     
