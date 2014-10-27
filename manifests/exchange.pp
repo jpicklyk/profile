8ff633937f802e22b2fcd3da5fc8fccb81c583c4
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-27 09:26:15 -0400

    Mounting iso code

--- manifests/exchange.pp
+++ manifests/exchange.pp
@@ -1,11 +1,25 @@
 class profile::exchange(
-  $exrole = hiera('profile::exchange::exrole'),
-  $share  = hiera('profile::exchange::share'),
+  $exrole       = hiera('profile::exchange::exrole'),
+  $share        = hiera('profile::exchange::share'),
+  $version      = hiera('profile::exchange::version', '2010'),
+  $servicepack  = hiera('profile::exchange::servicepack', 1)
 ){
   notify {'Applying profile: exchange':}
   class { 'exchange::prerequisites':
     exrole    => $exrole,
-    directory => $share,
+    directory => "${share}${version}\\",
   }
   contain exchange::prerequisites
+  
+  $filename = $servicepack ? {
+    1       => 'en_exchange_server_2010_with_sp1_x64_dvd_587827.iso',
+    default => fail('Unknown Exchange service pack')
+  }
+  
+  #Mount the iso
+  windows_isos{'Exchange 2010 SP1':
+    ensure  => present,
+    isopath => "${share}${version}\\${filename}"
+  }
+  contain windows_isos
 }
