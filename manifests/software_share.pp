4bc3149796752d78f8b4553fdfc2ee6ae3f7da80
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-28 08:49:43 -0400

    Can't use the name parameter

--- manifests/software_share.pp
+++ manifests/software_share.pp
@@ -2,7 +2,7 @@ class profile::software_share (
   $drive      = hiera('profile::software_share::drive'),
   $psprovider = hiera('profile::software_share::psprovider','FileSystem'),
   $root       = hiera('profile::software_share::root'),
-  $name       = hiera('profile::software_share::name', 'software'),
+  $sharename  = hiera('profile::software_share::sharename', 'software'),
   $account    = hiera("profile::software_share::account"),
   $key        = hiera("profile::software_share::key"),
 ) {
@@ -13,7 +13,7 @@ class profile::software_share (
 #    /^6\.1/ => "\$net = new-object -ComObject WScript.Network; \$net.MapNetworkDrive('${drive}:', '${root}', \$false, '${account}', '${key}')",
 #    default => "New-PSDrive -Name ${drive} -PSProvider ${psprovider} -Root ${root} -Credential ${credential} -Persist",
 #  }
-  $command = "cmdkey /add:${root} /user:${account} /pass:${key};net use ${drive}: '\\\\${root}\\${name}'"
+  $command = "cmdkey /add:${root} /user:${account} /pass:${key};net use ${drive}: '\\\\${root}\\${sharename}'"
   
   exec { 's-drive':
     command   => $command,
