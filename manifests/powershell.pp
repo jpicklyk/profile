dbff92c887021cddd64dac65e71cfcc571069618
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-30 14:07:08 -0400

    Patching in a new powershell installer

--- manifests/powershell.pp
+++ manifests/powershell.pp
@@ -1,6 +1,5 @@
-class profile::powershell(
-  $filename = hiera('profile::powershell::filename', 'Windows6.1-KB2819745-x64-MultiPkg.msu'),
-  $share    = hiera('profile::powershell::share')
+class profile::powershell(  
+  $version = hiera('profile::powershell::version')
 ){
   if($kernelversion =~ /^6\.1/) {
     notify{'Applying profile: powershell':}
@@ -14,15 +13,14 @@ class profile::powershell(
       when  => pending,
     }
     
-    exec {'powershell4':
-      command => "c:\\windows\\system32\\wusa.exe ${share}${filename} /quiet /norestart",
-      onlyif  => "c:\\windows\\system32\\WindowsPowershell\\v1.0\\powershell.exe -executionpolicy remotesigned if(\$PSVersionTable -and (\$PSVersionTable.PSVersion -ge [Version]'4.0')){exit 1}",
-      returns => [0,3010],
-      require => Reboot['before powershell']
+    class{'windowsps':
+      version => $version,
+      required => Reboot['before powershell']
     }
+    contain ::windowsps
     
     reboot{'after powershell':
-      subscribe => Exec['powershell4']
+      subscribe => Class['windowsps']
     } 
   }
 }
