2af7e04dea384115bc59d9f285e8d5cad5ad9483
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-31 11:23:51 -0400

    removing exchange profile

---
+++ manifests/exchange/unified.pp
@@ -0,0 +1,28 @@
+class profile::exchange::unified(
+  $exrole       = hiera('profile::exchange::exrole'),
+  $share        = hiera('profile::exchange::share'),
+  $version      = hiera('profile::exchange::version', '2010'),
+  $servicepack  = hiera('profile::exchange::servicepack', 1),
+  $orgname      = hiera('profile::exchange::orgname', 'Test Organization')
+){
+  debug('Applying profile: exchange')
+    
+  $sp = $servicepack ? {
+    1       => 'sp1',
+    default => fail('Unknown Exchange service pack')
+  }
+  
+  
+  class { 'exchange::prerequisites':
+    exrole    => $exrole,
+    directory => "${share}${version}\\",
+  } ->
+    
+  class {'exchange::install':
+    exrole    => $exrole,
+    path      => "${share}${version}\\${sp}\\",
+    orgname   => $orgname,
+  }
+  contain exchange::prerequisites  
+  contain exchange::install
+}
