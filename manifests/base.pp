13ba42c05f835ec56b584ab18e765cf044d2a725
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-11-03 16:46:10 -0500

    Adding shutdown script to environment

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,13 +1,17 @@
 class profile::base {
   notify {'Applying profile: base':}
+  #Run the shutdown profile for every OS.
+  contain profile::shutdown
+  
   if $operatingsystem == 'Windows' {
     contain profile::iesec
     #contain windows_puppet
     contain profile::win_default_apps
     contain profile::software_share
     contain profile::powershell
+    contain profile::shutdown
     
-    Class['profile::iesec'] -> Class['profile::software_share'] -> Class['profile::powershell'] -> Class['profile::win_default_apps'] 
+    Class['profile::iesec'] -> Class['profile::software_share'] -> Class['profile::powershell'] -> Class['profile::win_default_apps']
     
   } elsif $operatingsystem == 'Ubuntu' {
     
